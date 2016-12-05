50629a71d78309f1c8ce3acdf28164696cc52fb7
3olf <gachet.thomas@gmail.com>
2016-12-05 10:11:24 +0100

    ajout structure fichier lokisalle

---
+++ inc/git.init.inc.php
@@ -0,0 +1,11 @@
+<?php
+/* CONNEXION */
+$pdo = new PDO('mysql:host=localhost;dbname=;', '', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+
+/* SESSION */
+session_start();
+
+/* FUNCTIONS */
+require_once("functions.inc.php"); 
+
+$msg = "";
